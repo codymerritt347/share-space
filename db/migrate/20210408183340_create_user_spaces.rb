0c10b01dcc169de9fc0067aae17cb85763d0d767
codymerritt347 <cody.merritt@icloud.com>
2021-04-08 14:34:03 -0400

    Generate UserSpace model

---
+++ db/migrate/20210408183340_create_user_spaces.rb
@@ -0,0 +1,10 @@
+class CreateUserSpaces < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_spaces do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :space, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
