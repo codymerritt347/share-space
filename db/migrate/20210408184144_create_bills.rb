5c8c9ce88b050c4e83886ad0d3c11c3006e3aa3d
codymerritt347 <cody.merritt@icloud.com>
2021-04-08 14:44:48 -0400

    Fix create_bills migration

--- db/migrate/20210408184144_create_bills.rb
+++ db/migrate/20210408184144_create_bills.rb
@@ -4,9 +4,9 @@ class CreateBills < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :type
       t.belongs_to :space, null: false, foreign_key: true
-      t.string :float.amount
-      t.due_date :date
-      t.paid :boolean
+      t.float :amount
+      t.date :due_date
+      t.boolean :paid
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,65 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_08_184144) do
+
+  create_table "bills", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "space_id", null: false
+    t.float "amount"
+    t.date "due_date"
+    t.boolean "paid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["space_id"], name: "index_bills_on_space_id"
+  end
+
+  create_table "spaces", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "space_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["space_id"], name: "index_tasks_on_space_id"
+  end
+
+  create_table "user_spaces", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "space_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["space_id"], name: "index_user_spaces_on_space_id"
+    t.index ["user_id"], name: "index_user_spaces_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.string "gender"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bills", "spaces"
+  add_foreign_key "tasks", "spaces"
+  add_foreign_key "user_spaces", "spaces"
+  add_foreign_key "user_spaces", "users"
+end
